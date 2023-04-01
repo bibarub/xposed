c6b51c290772e473d96947c1eb507d8b8a8373e5
bibarub <wi1717171713@gmail.com>
2023-04-01 15:45:50 +0300

    trying to fix app install

--- service.sh
+++ service.sh
@@ -1,6 +1,5 @@
-until [[ $(getprop dev.bootcomplete) == "1" ]]; do sleep 1; done
-sleep 5
+sleep 15
 pm path de.robv.android.xposed.installer && exit
 cp -f ${0%/*}/XposedInstaller.apk /data/local/tmp
-pm install /data/local/tmp/XposedInstaller.apk &> /data/local/tmp/1.txt
+pm install /data/local/tmp/XposedInstaller.apk
 rm /data/local/tmp/XposedInstaller.apk
